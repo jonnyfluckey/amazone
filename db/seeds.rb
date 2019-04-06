60fbfb447a4b64f25c38a9ebf1ffed5f34b45d9f
Jonny Fluckey <jonathon.fluckey@gmail.com>
2019-04-06 12:14:05 -0600

    updated db:seeds, confirmed working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,20 @@
+require 'faker'
 
-
-20.times do
-  Department.create([{
-    name: Faker::Job.unique.field
-  }])
+20.times do |index|
+  Department.create!(
+    name: Faker::Job.unique.field,
+    user_id: 1
+  )
 end
 
-20.times do
-  Product.create([{
+20.times do |index|
+  Product.create!(
     name: Faker::GreekPhilosophers.name,
     description: Faker::Hipster.sentence,
     price: Faker::Number.decimal,
     stock: Faker::Number.non_zero_digit,
-    deparment_id: rand(1..5)
-  }])
+    department_id: rand(1..5)
+  )
   end
+
+  puts "your seed was successful"
