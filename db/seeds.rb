50b8ffdb86238663ae0f891f3d788d325b5563ab
Emily Johnson <emily.johnsonmsp@gmail.com>
2019-04-06 11:30:22 -0600

    add faker data in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+20.times do |index|
+  Department.create(
+    name: Faker::Job.unique.field
+  )
+end
+
+20.times do |index|
+  Product.create(
+    name: Faker::GreekPhilosophers.name
+    description: Faker::Hipster.sentence
+    price: Faker::Number.decimal
+    stock: Faker::Number.non_zero_digit
+    deparment_id: rand(1..5)
+  end
