e4823b658d8743cd10a41da71d001949ad7a62df
Tyson Duncan <Tyson.121@hotmail.com>
2019-04-06 12:16:35 -0600

    Product list component built

---
+++ client/src/components/ProductList.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Product from './Product';
+import ProductForm from './ProductForm';
+
+class ProductList extends Component {
+  state = { products: [] }
+
+  componentDidMount() {
+    axios.get('/api/products')
+      .then( res => {
+        this.setState({ products: res.data })
+      })
+      .catch( err => {
+        console.log(err)
+      })
+  }
+
+  displayProduct = () => {
+    return this.state.products.map( product => <Product key={product.id} {...product} />)
+  }
+
+  addProduct = ([product]) => {
+    axios.post('/api/products', { product })
+      .then( res => {
+        const { products } = this.state
+        this.setState({ products: [...products, res.data] })
+      })
+      .catch( err => {
+        console.log(err)
+      })
+  }
+
+  editProduct = (product) => {
+      axios.put(`/api/products/${product.id}`, {product})
+      .then( res => {
+          const products = this.state.products.map( p => {
+              if (p.id === post) 
+                return res.data
+              return p
+          })
+
+      })
+      .catch( err => {
+        console.log(err)
+      })
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Product List</h1>
+        <ProductForm add={this.addProduct} editProduct={this.editProduct}/>
+        { this.displayProduct() }
+      </>
+    )
+  }
+} 
+
+export default ProductList;
