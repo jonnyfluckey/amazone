248d4645ed060d86dd23a48ab1ecb81fe8c960c4
Emily Johnson <emily.johnsonmsp@gmail.com>
2019-04-06 11:40:33 -0600

    fix syntax for faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,17 @@
 require 'faker'
 
-20.times do |index|
-  Department.create(
+20.times do
+  Department.create([{
     name: Faker::Job.unique.field
-  )
+  }])
 end
 
-20.times do |index|
-  Product.create(
-    name: Faker::GreekPhilosophers.name
-    description: Faker::Hipster.sentence
-    price: Faker::Number.decimal
-    stock: Faker::Number.non_zero_digit
+20.times do
+  Product.create([{
+    name: Faker::GreekPhilosophers.name,
+    description: Faker::Hipster.sentence,
+    price: Faker::Number.decimal,
+    stock: Faker::Number.non_zero_digit,
     deparment_id: rand(1..5)
+  }])
   end
