a00ffd94d8074a7d246c283179555220d4fe032f
Tyson Duncan <Tyson.121@hotmail.com>
2019-04-06 10:53:47 -0600

    modified department controller

--- app/controllers/api/departments_controller.rb
+++ app/controllers/api/departments_controller.rb
@@ -1,2 +1,32 @@
 class Api::DepartmentsController < ApplicationController
+  def index
+    render json: Department.all
+  end
+
+  def create
+    @department = Department.new(dept_params)
+      if @department.save
+        render json: @department
+      else
+        render json: { errors: @department.errors }, status: :unprocessable_entity
+      end
+  end
+
+  def update
+    if @department.update(dept_params)
+      render json: @department
+    else
+      render json: {errors: @department.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    Department.find(params[:id]).destroy
+    render json: {message: 'Department has been deleted'}
+  end
+
+  private
+    def dept_params
+      params.require(:department).permit(:name)
+    end
 end
