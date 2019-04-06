a78bc486ee398e0f6ab137055d0e6aab815c50f7
Robert Morton <rlmorton83@gmail.com>
2019-04-06 11:05:39 -0600

    initial controller commit

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,33 @@
-class Api::ProductsController < ApplicationController
-end
+class Api::ProductsController < ProductsController
+    def index
+      render json: Product.all
+    end
+  
+    def create
+      product = Product.new(product_params)
+      if product.save
+        render json: product
+      else
+        render json: { errors: product.errors }, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      product = Product.find()params[:id]
+      # product.update( ) !product.complete )
+      render json: product
+    end
+  
+    def destroy  
+      Product.find(params[:id]).destroy
+      render json: { message: 'Product deleted' }
+    end
+  
+    private
+      def  product_params
+        params.require(:product).permit(:name, :description, :price, :stock)
+      end
+  
+  
+  end
+  
